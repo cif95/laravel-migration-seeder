0e13cb0c4ae8691c5f93e1cc3c5cb87fec03f209
cif95 <vivianacifarelli9@gmail.com>
2022-05-13 17:12:30 +0200

    change colors

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -19,9 +19,8 @@
 						<h5>Arrival</h5>
 						<p>{{ $train->arrival_station }} {{ $train->arrival_time }}</p>
 					</div>
-					<div class="info cell">
-						<h5>Detail</h5>
-						<p>{{ $train->train_code }}</p>
+					<div class="info cell txt-smaller">
+						<p>Train n.{{ $train->train_code }}</p>
 						<p>{{ $train->wagons }} wagons</p>
 						<p>{{ ($train->is_onScheduled) ? 'On time' : 'Delay'}}</p>
 						<p class="text-secondary">{{ ($train->is_cancelled) ? 'Cancelled' : 'Confirmed'}}</p>
