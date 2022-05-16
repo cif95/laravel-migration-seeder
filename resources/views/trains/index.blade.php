b07e1a207031d87c82a91c2d4cea978a084ac13f
cif95 <vivianacifarelli9@gmail.com>
2022-05-16 16:54:39 +0200

    enhanced show style

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -9,23 +9,31 @@
 				<div class="train bold">
 					<div class="departure cell">
 						<h5>Departure</h5>
-						<h4 class="text-secondary">
-							{{ date('d-m-Y', strtotime($train->departure_date)) }} 
-						</h4>
-						<h6 class="italic">{{ $train->company }}</h6>
-						<p>{{ $train->departure_station }}</p>
 						<p class="text-secondary">
-								{{ date('H:i', strtotime($train->departure_time)) }}
+							Date: {{ date('d-m-Y', strtotime($train->departure_date)) }} 
+						</p>
+						<h6 class="italic">
+							Company: {{ $train->company }}
+						</h6>
+						<p>Station: {{ $train->departure_station }}</p>
+						<p class="text-secondary">
+							Date: 
+							{{ date('H:i', strtotime($train->departure_time)) }}
 						</p> 
 					</div>
 					<div class="arrival cell">
-						<h5>Arrival</h5>
-						<p>{{ $train->arrival_station }} {{ date('H:i', strtotime($train->arrival_time)) }}</p>
+						<div>
+							<h5>Arrival</h5>
+							<p>Station: {{ $train->arrival_station }}</p>
+						</div>
+						<p class="text-secondary">
+							Time: {{ date('H:i', strtotime($train->arrival_time)) }}
+						</p>
 					</div>
 					<div class="info cell txt-smaller">
-						<p>Train n.{{ $train->train_code }}</p>
+						<h5>Train n.{{ $train->train_code }}</h5>
 						<p>{{ $train->wagons }} wagons</p>
-						<a class="btn secondary" href="{{ route('trains-detail', $id = $train->id) }}">View More</a>
+						<a class="btn secondary" href="{{ route('trains-detail', $id = $train->id) }}">Info</a>
 					</div>				
 				</div>
 			@endforeach
--- resources/views/trains/show.blade.php
+++ resources/views/trains/show.blade.php
@@ -4,19 +4,28 @@
 
 @section('content')
 	<section id="train-detail">
+		<a class="btn primary" href="{{ route('trains') }}">Back to Trains</a>
 		<div class="container primary">
 			<div class="train bold">
-				<h5>Departure {{ date('d-m-Y', strtotime($train->departure_date)) }} </h5>
-				<h6 class="italic">Company: {{ $train->company }}</h6>
-				<p>From {{ $train->departure_station }} at {{ date('H:i', strtotime($train->departure_time)) }}</p>
-				<h5>Arrival: </h5>
-				<p>{{ $train->arrival_station }} {{ date('H:i', strtotime($train->arrival_time)) }}</p>
+				<h4>Departure : {{ date('d-m-Y', strtotime($train->departure_date)) }} </h4>
+				<p class="italic">Company: {{ $train->company }}</p>
+				<p>
+					From {{ $train->departure_station }} 
+					at {{ date('H:i', strtotime($train->departure_time)) }}
+				</p>
+				<p>
+					Arrival at {{ $train->arrival_station }}
+					{{ date('H:i', strtotime($train->arrival_time)) }}
+				</p>
 				<p>Train n.{{ $train->train_code }}</p>
 				<p>{{ $train->wagons }} wagons</p>
-				<p>{{ ($train->is_onScheduled) ? 'On time' : 'Delay'}}</p>
-				<p class="text-secondary">{{ ($train->is_cancelled) ? 'Cancelled' : 'Confirmed'}}</p>				
+				<p>
+					Schedule: {{ ($train->is_onScheduled) ? 'On time' : 'Delay'}}
+				</p>
+				<p class="text-secondary">
+					Status: {{ ($train->is_cancelled) ? 'Cancelled' : 'Confirmed'}}
+				</p>				
 			</div>
-			<a class="btn primary" href="{{ route('trains') }}">Back to Trains</a>
 		</div>
 	</section>
 @endsection
